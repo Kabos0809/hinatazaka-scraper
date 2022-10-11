0e7a3488541b198792aa1aa3093b67d802c8f531
kabos <hiyamazu70@gmail.com>
2022-10-12 02:13:43 +0900

    小坂菜緒追加

--- Analysis/BarGraph.py
+++ Analysis/BarGraph.py
@@ -8,7 +8,7 @@ member_list = ["潮紗理菜", "影山優佳", "加藤史帆", "齊藤京子", "
                 "金村美玖", "河田陽菜", "小坂菜緒", "富田鈴花", "丹生明里", "濱岸ひより", "松田好花", "宮田愛萌", "上村ひなの",
                     "髙橋未来虹", "森本茉莉", "山口陽世"]
 
-def make_histogram(member):
+def make_bargraph(member):
     path = "../json/counts_json/all_media/{}_all_media_count.json".format(member)
     
     try:
@@ -48,4 +48,4 @@ def make_histogram(member):
     return
 
 for member in member_list:
-    make_histogram(member)
+    make_bargraph(member)
